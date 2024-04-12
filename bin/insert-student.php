c48ad1d7d42bad0c3a1e1ab9138cb6e0a5ab4aac
Klleriston <Klleandrade@outlook.com>
2024-04-12 15:42:34 -0300

    updated insert (CRUD METHOD)

--- bin/insert-student.php
+++ bin/insert-student.php
@@ -1,11 +1,25 @@
 <?php
+
+use Alura\Doctrine\Entity\Phone;
 use Alura\Doctrine\Entity\Student;
 use Alura\Doctrine\EntityManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManager = EntityManagerFactory::createEntityManager();
-$student = new Student($argv[1]);
 
+
+
+$phoneR = new Phone("(11) 2222 - 2222");
+$phoneP = new Phone("(11) 99999 - 9999");
+
+$entityManager->persist($phoneP);
+$entityManager->persist($phoneR);
+
+$student = new Student('Test');
 $entityManager->persist($student);
+
+$student->addPhone($phoneR);
+$student->addPhone($phoneP);
+
 $entityManager->flush();
